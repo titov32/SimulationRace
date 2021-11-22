3f97ea1edfa9fafa5c0980ab2891a8189c452392
titov32 <titov32@gmail.com>
2021-11-22 16:04:45 +0700

    refacoring code adding class aggregation

--- main.py
+++ main.py
@@ -1,38 +1,10 @@
-from car import Track, Car, Moto
-from config import LEN_CIRCLE, TRACKS, MOTOS, CARS
-from time import sleep
 
+from config import TRACKS, MOTOS, CARS
 
-def run():
-    the_end=False
-    racers = []
-    print("Помещаем на старт грузовики")
-    for track in TRACKS:
-        racers.append(Track(*TRACKS[track]))
-    print("Помещаем на старт лекговушки")
-    for car in CARS:
-        racers.append(Car(*CARS[car]))
-    print("Помещаем на старт мотоциклы")
-    for moto in MOTOS:
-        racers.append(Moto(*MOTOS[moto]))
-
-    for racer in racers:
-        racer.start()
-    print('_'*20)
-    print('Начинаем движение')
-    while not the_end:
-        for racer in racers:
-            racer.move()
-            if LEN_CIRCLE <= racer.odometer:
-                print(f'{racer} приехал первый')
-                the_end = True
-        sleep(5)
-        if the_end:
-            print('Гонка завершена')
-            answer = input('Напишите "1"  если продолжаем гонку')
-            if answer == '1':
-                the_end = False
+from race import Simulation
 
 
 if __name__ == '__main__':
-    run()
+    simulation = Simulation(TRACKS, MOTOS, CARS)
+    simulation.start()
+    simulation.run()
---
+++ race.py
@@ -0,0 +1,46 @@
+from car import Track, Car, Moto
+from time import sleep
+from config import LEN_CIRCLE
+
+
+class Simulation:
+    def __init__(self, tracks, motos, cars):
+        self.racers = []
+        self.the_end = False
+        self.road = Road()
+        self.winner = None
+        for track in tracks:
+            self.racers.append(Track(*tracks[track]))
+        for car in cars:
+            self.racers.append(Car(*cars[car]))
+        for moto in motos:
+            self.racers.append(Moto(*motos[moto]))
+
+    def start(self):
+        for racer in self.racers:
+            racer.start()
+        print('_' * 20)
+        print('Начинаем движение')
+
+    def run(self):
+        while not self.the_end:
+            for racer in self.racers:
+                racer.move()
+                sleep(0.5)
+                if LEN_CIRCLE <= racer.odometer:
+                    print(f'{racer} приехал первый')
+                    self.the_end = True
+                    self.winner = racer
+            print('__Прошел еще один отрезок времени__')
+            sleep(2)
+            if self.the_end:
+                print('Гонка завершена')
+                print(f'Победитель {self.winner}')
+                answer = input('Напишите "1"  если продолжаем гонку')
+                if answer == '1':
+                    self.the_end = False
+
+
+class Road:
+    def __init__(self):
+        self.len_circle = LEN_CIRCLE
